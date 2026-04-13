8ef1301e30b092b8dfec11e38d5e6948835aa705
WhisperedPanic <adam.k.oliver@protonmail.com>
2026-04-13 20:55:18 +0100

    Update stiffSolver.js

--- js/solvers/stiffSolver.js
+++ js/solvers/stiffSolver.js
@@ -35,10 +35,10 @@ function calculateDerivatives_SI(y, params) {
   }
   
   // CALIBRATED: Surface area multiplier
-  const surfaceMultiplier = 8.5;
+  const surfaceMultiplier = 1;
   
   // FINE-TUNED: B coefficient scaling
-  const B_SCALE_FACTOR = 2.4e7;
+  const B_SCALE_FACTOR = 1;
   
   // NO CLAMP - natural burn rate
   const r_burn_mps = propellant.B_mps_Pa_n * B_SCALE_FACTOR * Math.pow(P_MPa, n_eff);
